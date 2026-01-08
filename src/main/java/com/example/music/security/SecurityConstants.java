a10d99b8c68a5dc44d69174d79c31d2e3571bb57
Sheldon <147253966+sheldon34@users.noreply.github.com>
2026-01-08 18:17:56 +0300

    modifed secret key

--- src/main/java/com/example/music/security/SecurityConstants.java
+++ src/main/java/com/example/music/security/SecurityConstants.java
@@ -2,5 +2,5 @@ package com.example.music.security;
 
 public class SecurityConstants {
     public static final long JWT_EXPIRATION=   360000000;
-    public static final String JWT_SECRET="jwt_Secret";
+    public static final String JWT_SECRET="jwt_Secretsecret_key_needs_to_be_long_enough_for_hs512_algorithm_security_key_12345";
 }
